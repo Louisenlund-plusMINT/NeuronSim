5a8421d64a3bbb6a90ecf829c0a8d9c0a67b71ad
SkyWorld117 <alanzhu02@gmail.com>
2022-02-22 11:40:03 +0100

    fix bugs

--- network.jl
+++ network.jl
@@ -19,11 +19,11 @@ mutable struct Network
     function Network(;input_size::Int64, output_size::Int64, hidden_neurons::Int64, runtime::Int64)
         weights = rand(Float32, hidden_neurons+output_size, input_size+hidden_neurons)
         for i in 1:hidden_neurons
-            weights[input_size+i, hidden_neurons] = 0.0f0
+            weights[i, input_size+i] = 0.0f0
         end
         voltage = zeros(Float32, hidden_neurons+output_size, 1)
         output = zeros(Bool, input_size+hidden_neurons, 1)
-        fire_rec = fill(-refractory, hidden_neurons+output_size, 1)
+        fire_rec = zeros(Int64, hidden_neurons+output_size, 1)
 
         Pᵢ = zeros(Float32, hidden_neurons+output_size, input_size+hidden_neurons)
         M = zeros(Float32, hidden_neurons+output_size, 1)
@@ -31,13 +31,15 @@ mutable struct Network
         new(input_size, output_size, hidden_neurons, runtime, weights, voltage, output, fire_rec, Pᵢ, M, construct, activate)
     end
 
+    # n₂ => n₁
     function construct(self::Network; n₁::Int64, n₂::Int64)
-        self.weights[n₁+self.input_size, n₂] = (-1)^rand(Bool)*rand(Float32)
-        self.weights[n₂+self.input_size, n₁] = 0.0f0
+        self.weights[n₁, n₂+self.input_size] = (-1)^rand(Bool)*rand(Float32)
+        self.weights[n₂, n₁+self.input_size] = 0.0f0
     end
 
     # θ : threshold, r : refractory
     function activate(self::Network, spike_train::Array{Bool}; θ, r, η, γᵥ, γₚ, γₘ, A₊, A₋, w₊, w₋)
+        self.fire_rec = fill(-r, self.hidden_neurons+self.output_size, 1)
         for t in 1:self.runtime
             # load input data
             self.output[1:self.input_size, 1] .= spike_train[:,t]
@@ -56,20 +58,22 @@ mutable struct Network
             for i in 1:self.hidden_neurons+self.output_size
                 if t-self.fire_rec[i,1] > r 
                     self.Pᵢ[i,:] .+= self.output[:,1] .* (self.weights[i,:] .!= 0.0f0) * A₊
-                    self.weights[i,:] .+= η * self.output[:,1] .* (self.weights[i,:] .!= 0.0f0) * self.M[i,1] * (w₋ .- self.weights[i,:])
+                    self.weights[i,:] .+= η * (self.output[:,1] .* (self.weights[i,:] .!= 0.0f0))' * self.M[i,1] * (w₋ .- self.weights[i,:])
                 end
             end
 
             self.voltage .*= γᵥ
             self.voltage .= self.weights * self.output .* ((t .- self.fire_rec) .> r) + self.voltage
+            println(self.voltage[self.hidden_neurons+1:end])
             self.output[self.input_size+1:end, 1] .= self.voltage[1:self.hidden_neurons, 1] .>= θ
-            for i in axes(weights, 1) 
-                self.weights[i,:] .+= η * self.output[i,1] .* (self.weights[i,:] .!= 0.0f0) .* self.Pᵢ[i,:] * (w₊ .- self.weights[i,:])
+            for i in axes(self.weights, 1) 
+                self.weights[i,:] .+= η * (self.output[i,1] .* (self.weights[i,:] .!= 0.0f0))' .* self.Pᵢ[i,:] * (w₊ .- self.weights[i,:])
             end
 
             # update post-synaptic trace
             self.M .*= γₘ
-            self.M .-= (self.voltage[1:self.hidden_neurons, 1] .>= θ) * A₋
+            #self.M .-= (self.voltage[1:self.hidden_neurons, 1] .>= θ) * A₋
+            self.M .-= (self.voltage .>= θ) * A₋
         end
     end
 end
